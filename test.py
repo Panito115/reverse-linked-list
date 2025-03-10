5a1b3c05e9596888a22dc2e8d14ae825f845f493
Panito115 <jpmadriz@ufm.edu>
2025-03-10 11:17:43 -0600

    Imagenes y test

---
+++ test.py
@@ -0,0 +1,28 @@
+"""
+Test if reverse method of LL.
+"""
+
+
+
+from linked_list import Node, LinkedList
+
+node_a = Node("A")
+node_b = Node("B")
+node_c = Node("C")
+node_d = Node("D")
+
+ll = LinkedList()
+print(ll)
+
+#add nodes to LL
+
+ll.insert_at_end(node_a)
+ll.insert_at_end(node_b)
+ll.insert_at_end(node_c)
+ll.insert_at_end(node_d)
+print(ll)
+
+#test reverse() method
+
+ll.reverse()
+print(ll)
